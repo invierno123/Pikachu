c414374399f242dbd669928f15f27f93e5d9922d
invierno <1354002980@qq.com>
2020-12-05 20:01:06 +0800

    优化代码，构造播放器

--- main.js
+++ main.js
@@ -192,47 +192,65 @@ const string=`
   
 
 `
-let n=1
-demo.innerText=string.substr(0,n)
-demo2.innerHTML=string.substr(0,n)
 
-let time=100
-const fn=()=>{
-    n+=1
-    if(n>string.length){
-        window.clearInterval(id)
-        return
-    }
 
-    demo.innerText=string.substr(0,n)
-    demo2.innerHTML=string.substr(0,n)
-    demo.scrollTop=demo.scrollHeight
+const player={
+    id:undefined,
+    time:100,
+    n:1,
+    ui:{
+      demo:document.querySelector('#demo'),
+      demo2:document.querySelector('#demo2')
+    },
+    events:{
+        '#btnPause':'pause',
+        '#btnPlay':'play',
+        '#btnSlow':'slow',
+        '#btnNormal':'normal',
+        '#btnFast':'fast'
+    },
+    init:()=>{
+        player.ui.demo.innerText=string.substr(0,player.n)
+        player.ui.demo2.innerHTML=string.substr(0,player.n)
+        player.play()
+        player.bindEvents()
+    },
+    bindEvents:()=>{
+        for(let key in player.events){
+            if(player.events.hasOwnProperty(key)){
+                const value=player.events[key]
+                document.querySelector(key).onclick=player[value]
+            }
+        }
+    },
+    run:()=>{
+        player.n += 1
+        if(player.n>string.length){
+            window.clearInterval(id)
+            return
+        }
+        demo.innerText=string.substr(0,player.n)
+    player.ui.demo2.innerHTML=string.substr(0,player.n)
+    player.ui.demo.scrollTop=demo.scrollHeight
+    },
+    play:()=>{player.id=setInterval(player.run,player.time)},
+    pause:()=>{window.clearInterval(player.id) },
+    slow:()=>{ 
+    player.pause()
+    player.time=300
+    player.play()
+},
+normal:()=>{
+    player.pause()
+    player.time=100
+    player.play()
+},
+fast:()=>{
+    player.pause()
+    player.time=0
+    player.play()
 }
-const pause =()=>{
-    window.clearInterval(id)  
 }
-const play=()=>{
-    return setInterval(fn,time)
-}
-let id = play()
-btnPause.onclick=()=>{
-    pause()
-}
-btnPlay.onclick=()=>{
-    id=play()
-}
-btnSlow.onclick=()=>{
-    pause()
-    time=300
-    id=play()
-}
-btnNormal.onclick=()=>{
-    pause()
-    time=100
-    id=play()
-}
-btnFast.onclick=()=>{
-    pause()
-    time=0
-    id=play()
-}
+
+
+player.init()
